5f54806b134a5e9895f3f04116185c9437dcfd7d
Sean Cross <xobs@kosagi.com>
2012-06-20 23:05:52 +0800

    fpga: Make header work under C++

--- fpga.h
+++ fpga.h
@@ -1,5 +1,10 @@
-#ifndef __I2C_H__
-#define __I2C_H__
+#ifndef __FPGA_H__
+#define __FPGA_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <stdint.h>
 int read_fpga(uint8_t start_reg, void *buffer, int bytes);
 int write_fpga(uint8_t start_reg, void *buffer, uint32_t bytes);
@@ -9,4 +14,9 @@ int sync_fpga(void);
 
 uint32_t read_adc(uint32_t channel);
 uint32_t read_battery(void);
-#endif /* __I2C_H__ */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __FPGA_H__ */
