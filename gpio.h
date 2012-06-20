c5b94601ee9d622ae51981f4a35f4abf71f50688
Sean Cross <xobs@kosagi.com>
2012-06-21 02:57:10 +0800

    gpio: Make header file work under C++

--- gpio.h
+++ gpio.h
@@ -1,10 +1,18 @@
 #ifndef __GPIO_H__
 #define __GPIO_H__
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 int gpio_export(int gpio);
 int gpio_unexport(int gpio);
 int gpio_set_direction(int gpio, int is_output);
 int gpio_set_value(int gpio, int value);
 int gpio_get_value(int gpio);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // __GPIO_H__
