09e8f82e3197ae03b55565b236ff11e32cfb26fb
Sean Cross <smcross@gmail.com>
2012-06-25 16:55:06 +0800

    main: Wait for X11 to be running
    Blocks waiting for the Xserver to run, before starting the main
    Qt app.  This helps it work as a daemon, with systemd.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
 #include <QtGui/QApplication>
 #include "kovantestwindow.h"
 
+#ifdef Q_WS_X11
+void qt_x11_wait_for_window_manager(QWidget *widget);
+#endif
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     KovanTestWindow w;
     w.show();
     
+#ifdef Q_WS_X11
+	qt_x11_wait_for_window_manager(&w);
+#endif
+
     return a.exec();
 }
