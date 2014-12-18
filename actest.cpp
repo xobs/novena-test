de543e5a37494c00f5afc28277cdb56fbf75ba60
Sean Cross <xobs@kosagi.com>
2014-12-18 17:15:34 +0800

    actest: Implement test
    Implement ACTest, which is used to monitor the status of the AC plug
    and block until it has reached the defined state.
    
    Signed-off-by: Sean Cross <xobs@kosagi.com>

--- actest.cpp
+++ actest.cpp
@@ -1,4 +1,4 @@
-#include "buttontest.h"
+#include "actest.h"
 #include <QDir>
 #include <QFile>
 #include <QFileInfo>
@@ -18,38 +18,11 @@
 #define SENOKO_I2C_FILE "/dev/i2c-0"
 #define SENOKO_I2C_ADDR 0x20
 
-QFile *ButtonTest::openByName(const QString &name)
-{
-    char devName[256] = "???";
-    QDir inputDir("/dev/input");
+#define MAX_TRIES 10
 
-    inputDir.setNameFilters(QStringList() << "event*");
-    inputDir.setFilter(QDir::Files | QDir::Readable | QDir::System);
-    foreach (QFileInfo fileInfo, inputDir.entryInfoList()) {
-        QFile *inputFile = new QFile(fileInfo.absoluteFilePath());
-        if (!inputFile->open(QIODevice::ReadOnly)) {
-            testDebug(QString("Couldn't open %1: %2").arg(inputFile->fileName()).arg(inputFile->errorString()));
-            delete inputFile;
-            continue;
-        }
+static int senoko_fd;
 
-        ioctl(inputFile->handle(), EVIOCGNAME(sizeof(devName)), devName);
-        if (name == devName) {
-            testDebug(QString("Found %1").arg((const char*)devName));
-            int flags = fcntl(inputFile->handle(), F_GETFL);
-            flags |= O_NONBLOCK;
-            fcntl(inputFile->handle(), F_SETFL, flags);
-            return inputFile;
-        }
-        inputFile->close();
-        delete inputFile;
-    }
-
-    testDebug(QString("No files matched ") + name);
-    return NULL;
-}
-
-int ButtonTest::getSenokoRegister(int reg)
+static int getSenokoRegisterReal(int reg)
 {
     struct i2c_rdwr_ioctl_data session;
     struct i2c_msg messages[2];
@@ -83,113 +56,59 @@ int ButtonTest::getSenokoRegister(int reg)
     return ((int)data[0] & 0xff);
 }
 
-ButtonTest::ButtonTest(int _buttonMask)
+static int getSenokoRegister(int reg)
 {
-    name = "Button Test";
-    buttonMask = _buttonMask;
+    int tries;
+    bool success;
+    int ret = 0;
+
+    success = false;
+    for (tries = 0; !success && (tries < MAX_TRIES); tries++) {
+        ret = getSenokoRegisterReal(reg);
+        if (ret < 0)
+            sleep(1);
+        else
+            success = true;
+    }
+    if (!success)
+        return -1;
+    return ret;
+}
+
+ACTest::ACTest(enum direction _dir)
+{
+    if (_dir == PlugIn) {
+        name = "AC plug in test";
+        dir = PlugIn;
+    }
+    else {
+        name = "AC unplug test";
+        dir = UnPlug;
+    }
     senoko_fd = 0;
 }
 
-void ButtonTest::runTest()
+void ACTest::runTest()
 {
-    QFile *powerButton = NULL;
-    QFile *lidSwitch = NULL;
-    QFile *customButton = NULL;
-    QStringList buttonNames;
-    struct input_event evt;
+    if (dir == PlugIn)
+        testInfo("Waiting for AC to be connected");
+    else
+        testInfo("Waiting for AC to be unplugged");
 
-    if (buttonMask & PowerButton) {
-        powerButton = openByName("Senoko keypad");
-        if (!powerButton) {
-            testInfo("Couldn't find Senoko keypad, will poll");
-        }
-        buttonNames.append("Power button");
-    }
+    while (1) {
 
-    if (buttonMask & ACPlug) {
-        buttonNames.append("AC plug");
-    }
+        int ret = getSenokoRegister(0x0f);
+        if (ret < 0)
+            continue;
 
-    if (buttonMask & LidSwitch) {
-        lidSwitch = openByName("gpio-keys");
-        if (!lidSwitch) {
-            testError("Couldn't find gpio-keys");
-            return;
-        }
-        buttonNames.append("Lid switch");
-    }
-
-    if (buttonMask & CustomButton) {
-        customButton = openByName("20b8000.kpp");
-        if (!customButton) {
-            testError("Couldn't find 20b8000.kpp");
-            return;
-        }
-        buttonNames.append("User button");
-    }
-
-    testInfo(QString("Please trigger these buttons: ").append(buttonNames.join(",")));
-
-    int loops = 0;
-    while (buttonMask) {
-        loops++;
-
-        if ((buttonMask & PowerButton) && powerButton) {
-            memset((void *)&evt, 0, sizeof(evt));
-            int ret = read(powerButton->handle(), (char *)&evt, sizeof(evt));
-            if (ret == sizeof(evt)) {
-                if ((evt.type == EV_KEY) && (evt.code == KEY_POWER)) {
-                    testInfo("Got Power key (evdev)");
-                    buttonMask &= ~PowerButton;
-                }
-                else if ((evt.type == EV_MSC) && (evt.code == MSC_SCAN))
-                    testDebug("Got keypad scan (custom key should be coming up soon)");
-                else
-                    testInfo(QString("Got unknown key from Senoko: Type: %1  Code: %2").arg(QString::number(evt.type, 16)).arg(QString::number(evt.code, 16)));
-            }
+        if ((dir == PlugIn) && (ret & (1 << 3))) {
+            testInfo("AC connected");
+            break;
         }
 
-        if ((buttonMask & PowerButton)) {
-            int ret = getSenokoRegister(0x0f);
-            if (ret >= 0 && (ret & (1 << 4))) {
-                testInfo("Got Power key (i2c direct)");
-                buttonMask &= ~PowerButton;
-            }
-        }
-
-        if (buttonMask & ACPlug) {
-            int ret = getSenokoRegister(0x0f);
-            if (ret >= 0 && !(ret & (1 << 3))) {
-                testInfo("Got AC unplug (i2c direct)");
-                buttonMask &= ~ACPlug;
-            }
-        }
-
-        if ((buttonMask & LidSwitch) && lidSwitch) {
-            memset((void *)&evt, 0, sizeof(evt));
-            int ret = read(lidSwitch->handle(), (char *)&evt, sizeof(evt));
-            if (ret == sizeof(evt)) {
-                if ((evt.type == EV_SW) && (evt.code == SW_LID)) {
-                    testInfo("Got Lid switch");
-                    buttonMask &= ~LidSwitch;
-                }
-                else
-                    testInfo(QString("Got unknown key from GPIO: Type: %1  Code: %2").arg(QString::number(evt.type, 16)).arg(QString::number(evt.code, 16)));
-            }
-        }
-
-        if ((buttonMask & CustomButton) && customButton) {
-            memset((void *)&evt, 0, sizeof(evt));
-            int ret = read(customButton->handle(), (char *)&evt, sizeof(evt));
-            if (ret == sizeof(evt)) {
-                if ((evt.type == EV_KEY) && (evt.code == KEY_CONFIG)) {
-                    testInfo("Got Custom key");
-                    buttonMask &= ~CustomButton;
-                }
-                else
-                    testInfo(QString("Got unknown key from KPP: Type: %1  Code: %2").arg(QString::number(evt.type, 16)).arg(QString::number(evt.code, 16)));
-            }
+        else if ((dir == UnPlug) && !(ret & (1 << 3))) {
+            testInfo("AC disconnected");
+            break;
         }
     }
-    testInfo("Got all buttons");
 }
